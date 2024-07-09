0d486520d63348356f59da4d395805d86eeb5e02
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-07-09 12:15:40 +0300

    update path

--- routers/member.router.js
+++ routers/member.router.js
@@ -39,19 +39,6 @@ const upload=multer({
 })
 
 
-// Router.route("/book")
-//         .post(upload.single("file"),async(req,res)=>{
-//                 const {title }=req.body;
-//                 const file =req.file;
-//                 console.log("file data is : ",file);
-//                 console.log("body data is : ",req.url);
-//                 const newBook=await  new book({
-//                         title,
-//                         url:"http://localhost:3000/book/"+file.filename
-//                 })
-//                 newBook.save();
-//                 res.end("done");
-//         })
 
 Router.route("/verifyEmail")
         .post(memberControler.verifyEmail)
