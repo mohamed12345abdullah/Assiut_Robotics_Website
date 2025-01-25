0002b03b8f4e9a28f991fd0efaf508f3337326c4
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-01-25 14:04:52 +0200

    push

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 const express=require('express');
-
+const path =require("path")
 
 const app=express();
 
 
-app.use("/", express.static("./public"));
+app.use("/", express.static(__dirname+"/public"));
 
 app.listen(3000,()=>{
 
