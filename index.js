6469ab539162ad319a7b48be811662c1d2ddc822
Antony Ghayes <121578807+Antony-Gh@users.noreply.github.com>
2025-01-28 19:29:36 +0200

    aga 4

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 
-const express=require('express');
-const path =require("path")
+const express = require('express');
+const path = require("path")
 
-const app=express();
-
-
-app.use("/", express.static(__dirname+"/public"));
-
-app.listen(3000,()=>{
+const app = express();
 
 
-    console.log("server run ");
-    
+app.use("/", express.static(__dirname + "/public"));
+
+app.listen(5000, () => {
+    console.log("server is run and listen to port : ", `http://localhost:${5000}/`);
 })
