f7a888bf5eaea44380158d2e8fb010d4a83ab6a7
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-01-31 14:22:31 +0200

    update link

--- public/memberPage/script.js
+++ public/memberPage/script.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = 'http://localhost:3000/members/getAllMembers';
+const API_URL = 'https://assiut-robotics-zeta.vercel.app/members/getAllMembers';
 
 // DOM Elements
 const leaderSection = document.getElementById('leaderSection');
