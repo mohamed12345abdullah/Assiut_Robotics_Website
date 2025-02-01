0acadec6d7fac13000fde5c72237dc9aa3f0b44c
mazen magdy <69149378+Mazen-magdy@users.noreply.github.com>
2025-02-01 22:46:34 +0200

    make btn for management

--- public/profile-page/app.js
+++ public/profile-page/app.js
@@ -357,7 +357,29 @@ function renderTasks(tasks) {
     tasksList.appendChild(taskElement);
   });
 }
-
+// Manage btn
+function manage(){
+  const managebtn = document.getElementById('manage');
+  const data = JSON.parse(localStorage.getItem('data'));
+  if(data.role == "head" || data.role == "leader")
+  {
+    managebtn.style.display = "block";
+    if(data.role == 'head')
+    {
+      managebtn.addEventListener('click',()=>{
+        window.location.href = "../head/index.html";
+      })
+    }
+    else if(data.role == 'leader')
+    {
+      managebtn.addEventListener('click',()=>{
+        window.location.href = "../leader/index.html";
+      })
+    }
+  }
+  else
+    managebtn.style.display = "none";
+}
 
 
 
