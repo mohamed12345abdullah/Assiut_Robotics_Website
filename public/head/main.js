c9c9db1b0678eee417692a7a4437a97ca63a3769
mahmoudvs183 <mahmoudvs183@gmail.com>
2025-02-15 21:14:51 +0200

    update Hr Evaluation Section

--- public/head/main.js
+++ public/head/main.js
@@ -680,3 +680,74 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e =
 
 // Initialize
 fetchMembers();
+
+// HrEvaluationSection
+async function fetchMembersForHr() {
+  
+    // localStorage.setItem("data", JSON.stringify({ role: "HR web" }));
+    let userData = localStorage.getItem("data");
+    let parsedData = JSON.parse(userData);
+    let hrRole = parsedData.role;
+    let words = hrRole.split(" "); 
+    let hrCommitte = words.length > 1 ? words[1] : "Not found";
+    // console.log(hrCommitte);
+    if(hrCommitte !="Not found"){
+        try {
+      const response = await fetch(`https://assiut-robotics-zeta.vercel.app/members/get/${hrCommitte}`);
+      const data = await response.json();
+      let members = data.date;
+  
+      // console.log(members);
+      let memberSelect = document.getElementById("memberId");
+      memberSelect.innerHTML = '<option value=""> Select Member</option>';
+      members.forEach(member => {
+        memberSelect.innerHTML += `<option value="${member._id}">${member.name}</option>`;
+      });
+  
+    } catch (error) {
+      console.error('Error fetching members:', error);
+    }
+    }
+  }
+  
+  fetchMembersForHr();
+  
+  document.getElementById('evaluationForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+  
+  
+    const formData = {
+      month: document.getElementById('month').value,
+      memberId: document.getElementById('memberId').value,
+      socialScore: Number(document.getElementById('socialScore').value),
+      behaviorScore: Number(document.getElementById('behaviorScore').value),
+      interactionScore: Number(document.getElementById('interactionScore').value)
+    };
+    const messageDiv = document.getElementById('message');
+    // console.log(JSON.stringify(formData));
+  
+    try {
+      const response = await fetch('https://assiut-robotics-zeta.vercel.app/members/update-tasks-evaluation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+  
+      if (response.ok) {
+        messageDiv.textContent = 'Evaluation submitted successfully';
+        messageDiv.className = 'message success';
+        e.target.reset();
+  
+        setTimeout(() => {
+          messageDiv.style.display = 'none';
+        }, 5000);
+      } else {
+        throw new Error('Error submitting evaluation');
+      }
+    } catch (error) {
+      messageDiv.textContent = 'Error submitting evaluation';
+      messageDiv.className = 'message error';
+    }
+  });
