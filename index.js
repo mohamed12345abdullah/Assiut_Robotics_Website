1d8b7675d670c7695edf752fed2a4db88d9635c0
CodeMInds-sys <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-03-11 21:49:16 +0200

    fire our website

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require("path")
 
 const app = express();
 
-app.use("/",express.static(path.join(__dirname,"public")))
+
+app.use("/", express.static(__dirname + "/public"));
 
 app.listen(5000, () => {
     console.log("server is run and listen to port : ", `http://localhost:${5000}/`);
