73fb3c1bec2dea206aae2cb77fba27337afcb225
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-03-12 22:44:52 +0200

    Update app.js error in change profile image

--- public/profile-page/app.js
+++ public/profile-page/app.js
@@ -154,7 +154,7 @@ async function changeAvatar(file) {
     }
   } catch (error) {
     console.error('Error changing avatar:', error);
-    alert('Error changing avatar. Please try again later.');
+    alert('Error changing avatar.',error.message);
   }
 }
 
