6d64678d58c4f6ee67944034b06744abc78fa465
mazen magdy <69149378+Mazen-magdy@users.noreply.github.com>
2025-04-14 11:37:33 +0200

    add pagenation

--- public/OC_page/component.js
+++ public/OC_page/component.js
@@ -10,14 +10,42 @@ const getComponents = async () => {
         components = res.data;
         console.log(components);
 
-        components.forEach(element => {
-            container.innerHTML += `
-            <div class="component box" ">
-            <img src="${element.image}" alt="">
-            <div class="name"> ${element.title}</div>
-            
-        </div>`
-        });
+        // make taps
+        let step = (window.innerWidth > 1000 ? 16 : 8);
+        const taps = document.querySelector(".taps");
+        // calculate the number of taps
+        for (let i = 0; i < Math.ceil(components.length / step) ; i++) {
+            taps.innerHTML += `<button id="${i}">${i + 1}</button>`;
+        }
+        const btns = document.querySelectorAll(".taps button");
+        console.log(btns);
+
+        // add event listener to each tap
+        btns.forEach(button => {
+            button.addEventListener("click", () => {
+                console.log(button.id);
+                // remove active class from all taps
+                btns.forEach(btn => {
+                    btn.classList.remove("Active");
+                });
+                // add active class to the clicked tap
+                button.classList.add("Active");
+                // clear the container
+                container.innerHTML = "";
+                // loop through the components
+                for(let i = button.id * step; i < Math.min(step * (Number(button.id) + 1), components.length); i++) {
+                    console.log(i);
+                    container.innerHTML += `
+                        <div class="component box" ">
+                        <img src="${components[i].image}" alt="">
+                        <div class="name"> ${components[i].title}</div>
+                        </div>
+                    `
+                }
+            })
+        })
+        // Click the first button by default
+        btns[0].click();
 
     } else {
         console.log(response);
